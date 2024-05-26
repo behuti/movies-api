a0b83f7ebe82a3394e6c76594f4be36d7c587736
behuti <echenao@gmail.com>
2024-05-26 15:29:24 -0500

    add image response

--- 1.http.js
+++ 1.http.js
@@ -1,10 +1,22 @@
 // PROTOCOLO HTTP
 const http = require('node:http')
+const fs = require('node:fs')
 
 const processRequest = (req, res) => {
   res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
   if (req.url === '/') {
     res.end('<h1>Bienvenido a mi página de inicio.</h1>')
+  } else if (req.url === '/imagen-bonita.jpg') {
+    fs.readFile('./eye.jpg', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>Error al cargar la imagen</h1>')
+      } else {
+        res.setHeader('content-type', 'image/jpg')
+        res.end(data)
+      }
+    })
   } else if (req.url === '/contacto') {
     res.end('<h1>Contacto</h1>')
   } else {
