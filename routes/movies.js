2e2803c5a43287af6bda35ed285282a659404dfe
behuti <echenao@gmail.com>
2024-06-08 22:57:51 -0500

    create movies controller

--- routes/movies.js
+++ routes/movies.js
@@ -1,62 +1,11 @@
 import { Router } from 'express'
-import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
-
-import { MovieModel } from '../models/movie.js'
+import { MovieController } from '../controllers/movies.js'
 
 export const moviesRouter = Router()
 
-moviesRouter.get('/', async (req, res) => {
-  const { genre } = req.query
-  const movies = await MovieModel.getAll({ genre })
-  res.json(movies)
-})
+moviesRouter.get('/', MovieController.getAll)
+moviesRouter.post('/', MovieController.create)
 
-moviesRouter.get('/:id', async (req, res) => {
-  const { id } = req.params
-  const movie = await MovieModel.getById({ id })
-  if (movie) return res.json(movie)
-  res.status(404).json({ error: 'Movie not found' })
-})
-
-moviesRouter.post('/', async (req, res) => {
-  // Validation
-  const result = validateMovie(req.body)
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const newMovie = await MovieModel.create({ input: result.data })
-
-  res.status(201).json(newMovie) // Update the cache status
-})
-
-moviesRouter.delete('/:id', async (req, res) => {
-  const { id } = req.params
-
-  const result = await MovieModel.delete({ id })
-
-  if (result === false) {
-    return res.status(404).json({ error: 'Movie not found' })
-  }
-
-  return res.json({ message: 'Movie deleted' })
-})
-
-moviesRouter.patch('/:id', async (req, res) => {
-  // Validation
-  const result = validatePartialMovie(req.body)
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const { id } = req.params
-  const updatedMovie = await MovieModel.update({ id, input: result.data })
-
-  if (updatedMovie === false) {
-    return res.status(404).json({ error: 'Movie not found' })
-  }
-
-  return res.json(updatedMovie)
-})
+moviesRouter.get('/:id', MovieController.getById)
+moviesRouter.delete('/:id', MovieController.delete)
+moviesRouter.patch('/:id', MovieController.update)
