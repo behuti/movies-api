d21d3f43f3d22279f270721a76cd6ae839c60459
behuti <echenao@gmail.com>
2024-06-10 21:44:52 -0500

    add sql join by genre

--- models/mysql/movie.js
+++ models/mysql/movie.js
@@ -12,6 +12,21 @@ const connection = await mysql.createConnection(config)
 
 export class MovieModel {
   static async getAll({ genre }) {
+    if (genre) {
+      const [genres] = await connection.query(
+        'SELECT id, name FROM genre WHERE LOWER(name) = ?;',
+        [genre.toLowerCase()]
+      )
+
+      if (genres.length === 0) return []
+      const [{ id }] = genres
+      const [moviesByGenre] = await connection.query(
+        'SELECT BIN_TO_UUID(movie_id) AS id, title, year, director, duration, poster, rate FROM movie INNER JOIN movie_genre AS mg ON movie_id = mg.movie_id WHERE mg.genre_id = ?;',
+        [id]
+      )
+      return moviesByGenre
+    }
+
     const [movies] = await connection.query(
       'SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id FROM movie'
     )
