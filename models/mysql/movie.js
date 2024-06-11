e381f7a06c24ea274ef7ad210e9b41517a387d32
behuti <echenao@gmail.com>
2024-06-10 22:16:13 -0500

    Create sql

--- models/mysql/movie.js
+++ models/mysql/movie.js
@@ -34,9 +34,55 @@ export class MovieModel {
     return movies
   }
 
-  static async getById({ id }) {}
+  static async getById({ id }) {
+    const [movies] = await connection.query(
+      `SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id
+      FROM movie WHERE id = UUID_TO_BIN(?)`,
+      [id]
+    )
 
-  static async create({ input }) {}
+    if (movies.length === 0) return null
+    return movies[0]
+  }
+
+  static async create ({ input }) {
+    const {
+      genre: genreInput, // genre is an array
+      title,
+      year,
+      duration,
+      director,
+      rate,
+      poster
+    } = input
+
+    // todo: crear la conexión de genre
+
+    // crypto.randomUUID()
+    const [uuidResult] = await connection.query('SELECT UUID() uuid;')
+    const [{ uuid }] = uuidResult
+
+    try {
+      await connection.query(
+        `INSERT INTO movie (id, title, year, director, duration, poster, rate)
+          VALUES (UUID_TO_BIN("${uuid}"), ?, ?, ?, ?, ?, ?);`,
+        [title, year, director, duration, poster, rate]
+      )
+    } catch (e) {
+      // puede enviarle información sensible
+      throw new Error('Error creating movie')
+      // enviar la traza a un servicio interno
+      // sendLog(e)
+    }
+
+    const [movies] = await connection.query(
+      `SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id
+        FROM movie WHERE id = UUID_TO_BIN(?);`,
+      [uuid]
+    )
+
+    return movies[0]
+  }
 
   static async delete({ id }) {}
 
